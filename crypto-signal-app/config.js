7065cec51fea794f3d0a212b7c7855008d0ccd90
dinorebac-code <dinorebac@gmail.com>
2026-04-09 19:10:26 +0200

    Add safe config template

---
+++ crypto-signal-app/config.js
@@ -0,0 +1,13 @@
+window.APP_CONFIG = {
+  supabaseUrl: "",
+  supabaseAnonKey: "",
+  twelveDataApiKey: "",
+  notificationEmail: "your@email.com",
+  timezone: "Europe/Oslo",
+  watchWindow: {
+    start: "16:30",
+    end: "17:30",
+    pollSeconds: 45,
+  },
+  demoMode: true,
+};
